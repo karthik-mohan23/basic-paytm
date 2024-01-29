04dfc836b99ba6c56a2cd955ea492f4abcec0a16
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-01-30 00:10:27 +0530

    setup user routes and installed jsonwebtoken

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const rootRouter = require("./routes/index");
+
+app.use(express.json());
+
+app.use("/api/v1", rootRouter);
+
 (async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
