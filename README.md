c4e5fbfc0524bc3ca6ad41e9529ca77070fd64d4
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-01-29 23:37:37 +0530

    connected to DB and created User model

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 ## Build a basic version of PayTM
-
-<!-- simpletodo -->
-
-<!-- mongodb+srv://karthik1:<password>@cluster0.jxqbm2p.mongodb.net/?retryWrites=true&w=majority -->
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
+require("dotenv").config();
 const express = require("express");
+const app = express();
 
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
 
+    app.listen(process.env.PORT, () => {
+      console.log("Server is listening at " + process.env.PORT);
+    });
+  } catch (error) {
+    console.console.log("Something went wrong!!!");
+    console.error(error);
+  }
+})();
