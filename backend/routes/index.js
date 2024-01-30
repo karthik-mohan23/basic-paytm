dc57b3a26b369b9ba49ef136b9cbf3648762fc8d
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-01-30 15:27:40 +0530

    created  endpoint for user to transfer money to another account

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const userRouter = require("./user");
+const accountsRouter = require("./accounts");
 
 router.use("/user", userRouter);
+router.use("/accounts", accountsRouter);
 
 module.exports = router;
